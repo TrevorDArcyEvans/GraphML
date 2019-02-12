d42eb64e5d6b56d49b678512ab511e3af5713b30
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-02-12 15:33:55 +0000

    - added config file

--- GraphML.UI.Desktop/Main.cs
+++ GraphML.UI.Desktop/Main.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace GraphML.UI.Desktop
 {
   public partial class Main : Form
   {
+    private readonly string _server;
+    private readonly string _userName;
+    private readonly string _password;
+
     public Main()
     {
       InitializeComponent();
+
+      _server = ConfigurationManager.AppSettings["GraphML_Server"];
+      _userName = ConfigurationManager.AppSettings["GraphML_UserName"];
+      _password = ConfigurationManager.AppSettings["GraphML_Password"];
     }
   }
 }
