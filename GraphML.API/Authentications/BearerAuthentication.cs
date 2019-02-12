f73d69d605b9e887c4c402518ea3c885d86b8e44
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-02-12 13:41:06 +0000

    - revisit bearer authentication when we need it

--- GraphML.API/Authentications/BearerAuthentication.cs
+++ GraphML.API/Authentications/BearerAuthentication.cs
@@ -2,7 +2,6 @@
 using GraphML.Utils;
 using IdentityModel.Client;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
@@ -30,41 +29,7 @@ namespace GraphML.API.Authentications
 
     public async Task Authenticate(TokenValidatedContext context)
     {
-      // set roles based on email-->organisation-->org.PrimaryRoleId
-      var bearerToken = context.SecurityToken.SecurityKey.ToString();
-
-      // have to cache responses or UserInfo endpoint thinks we are a DOS attack
-      if (_cache.TryGetValue(bearerToken, out CachedUserInfoResponse cachedresponse))
-      {
-        if (cachedresponse.Created < DateTime.UtcNow.Subtract(Expiry))
-        {
-          _cache.Remove(bearerToken);
-          cachedresponse = null;
-        }
-      }
-
-      var response = cachedresponse?.UserInfoResponse;
-      if (response == null)
-      {
-        var userInfoClient = new UserInfoClient(Settings.OIDC_USERINFO_URL(_config));
-        response = await userInfoClient.GetAsync(bearerToken.Substring(7));
-        _cache.Add(bearerToken, new CachedUserInfoResponse(response));
-      }
-
-      var userClaims = response.Claims;
-      var claims = new List<Claim>(userClaims);
-
-      // ClaimTypes.Email --> 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'
-      // but some OIDC providers use 'email'
-      var email = userClaims.SingleOrDefault(x =>
-        x.Type == ClaimTypes.Email ||
-        x.Type.ToLowerInvariant() == "email")?.Value;
-      if (!string.IsNullOrEmpty(email))
-      {
-        claims.Add(new Claim(ClaimTypes.Email, email));
-      }
-
-      context.Principal.AddIdentity(new ClaimsIdentity(claims));
+      throw new NotImplementedException();
     }
 
     private sealed class CachedUserInfoResponse
