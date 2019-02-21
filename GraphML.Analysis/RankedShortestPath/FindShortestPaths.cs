1b35ee38d1ef9612a2c011e29cdb43cb4d747eef
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-02-21 17:41:26 +0000

    - expose ShortestPathCount as a property

--- GraphML.Analysis/RankedShortestPath/FindShortestPaths.cs
+++ GraphML.Analysis/RankedShortestPath/FindShortestPaths.cs
@@ -11,17 +11,19 @@ namespace GraphML.Analysis.RankedShortestPath
     private readonly IBidirectionalGraph<TVertex, TEdge> _graph;
     private readonly Func<TEdge, double> _edgeWeights;
 
+    public int ShortestPathCount { get; set; } = 10;
+
     public FindShortestPaths(IBidirectionalGraph<TVertex, TEdge> graph, Func<TEdge, double> edgeWeights)
     {
       _graph = graph;
       _edgeWeights = edgeWeights;
     }
 
-    public IEnumerable<FindShortestPathsResult<TEdge>> GetShortestPaths(TVertex rootVertex, TVertex goalVertex, int maxCount)
+    public IEnumerable<FindShortestPathsResult<TEdge>> GetShortestPaths(TVertex rootVertex, TVertex goalVertex)
     {
       var algo = new HoffmanPavleyRankedShortestPathAlgorithm<TVertex, TEdge>(_graph, _edgeWeights)
       {
-        ShortestPathCount = maxCount
+        ShortestPathCount = ShortestPathCount
       };
       algo.Compute(rootVertex, goalVertex);
 
