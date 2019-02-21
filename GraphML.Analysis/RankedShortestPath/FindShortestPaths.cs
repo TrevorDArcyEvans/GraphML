a00302cd76c6e52f4508769c7ea7a06c4a285cd9
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-02-21 17:37:48 +0000

    - added FindShortestPaths

---
+++ GraphML.Analysis/RankedShortestPath/FindShortestPaths.cs
@@ -0,0 +1,31 @@
+﻿using QuickGraph;
+using QuickGraph.Algorithms.RankedShortestPath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphML.Analysis.RankedShortestPath
+{
+  public sealed class FindShortestPaths<TVertex, TEdge> where TEdge : IEdge<TVertex>
+  {
+    private readonly IBidirectionalGraph<TVertex, TEdge> _graph;
+    private readonly Func<TEdge, double> _edgeWeights;
+
+    public FindShortestPaths(IBidirectionalGraph<TVertex, TEdge> graph, Func<TEdge, double> edgeWeights)
+    {
+      _graph = graph;
+      _edgeWeights = edgeWeights;
+    }
+
+    public IEnumerable<FindShortestPathsResult<TEdge>> GetShortestPaths(TVertex rootVertex, TVertex goalVertex, int maxCount)
+    {
+      var algo = new HoffmanPavleyRankedShortestPathAlgorithm<TVertex, TEdge>(_graph, _edgeWeights)
+      {
+        ShortestPathCount = maxCount
+      };
+      algo.Compute(rootVertex, goalVertex);
+
+      return algo.ComputedShortestPaths.Select(path => new FindShortestPathsResult<TEdge>(path, path.Sum(e => _edgeWeights(e))));
+    }
+  }
+}
---
+++ GraphML.Analysis/RankedShortestPath/FindShortestPathsResult.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace GraphML.Analysis.RankedShortestPath
+{
+  public sealed class FindShortestPathsResult<TEdge>
+  {
+    public IEnumerable<TEdge> Path { get; }
+    public double Cost { get; }
+
+    public FindShortestPathsResult(IEnumerable<TEdge> path, double cost)
+    {
+      Path = path;
+      Cost = cost;
+    }
+  }
+}
