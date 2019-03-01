dbc796e84cae0c5c06101450a74d9ea50097dcb6
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-03-01 15:46:06 +0000

    - inject logger

--- GraphML.Analysis.SNA/Centrality/DegreeJob.cs
+++ GraphML.Analysis.SNA/Centrality/DegreeJob.cs
@@ -1,19 +1,24 @@
 ﻿using GraphML.Interfaces;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Threading;
 
 namespace GraphML.Analysis.SNA.Centrality
 {
   public sealed class DegreeJob : JobBase, IDegreeJob
   {
     private readonly IConfiguration _config;
+    private readonly ILogger<DegreeJob> _logger;
     private readonly IGraphDatastore _graphDatastore;
 
     public DegreeJob(
       IConfiguration config,
+      ILogger<DegreeJob> logger,
       IGraphDatastore graphDatastore)
     {
       _config = config;
+      _logger = logger;
       _graphDatastore = graphDatastore;
     }
 
@@ -21,6 +26,9 @@ namespace GraphML.Analysis.SNA.Centrality
     {
       var degReq = (DegreeRequest)req;
       Console.WriteLine($"DegreeJob.Run --> {degReq.GraphId} @ {degReq.CorrelationId}");
+
+      // simulates a log running process
+      Thread.Sleep(TimeSpan.FromMinutes(10));
     }
   }
 }
