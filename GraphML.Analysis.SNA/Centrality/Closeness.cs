53aef2fa662824ea8f28bdac60f7b2d3e72bf321
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-03-04 15:54:29 +0000

    - removed support for IComputation as it was untested

--- GraphML.Analysis.SNA/Centrality/Closeness.cs
+++ GraphML.Analysis.SNA/Centrality/Closeness.cs
@@ -1,5 +1,4 @@
 ﻿using QuickGraph;
-using QuickGraph.Algorithms;
 using QuickGraph.Algorithms.Observers;
 using QuickGraph.Algorithms.ShortestPath;
 using System;
@@ -8,21 +7,14 @@ using System.Threading;
 
 namespace GraphML.Analysis.SNA.Centrality
 {
-  public sealed class Closeness<TVertex, TEdge> : IComputation, ICentralityClosenessAlgorithm<TVertex> where TEdge : IEdge<TVertex>
+  public sealed class Closeness<TVertex, TEdge> : ICentralityClosenessAlgorithm<TVertex> where TEdge : IEdge<TVertex>
   {
     private readonly IVertexListGraph<TVertex, TEdge> _graph;
-    Func<TEdge, double> _weights;
-
-    public event EventHandler StateChanged;
-    public event EventHandler Started;
-    public event EventHandler Finished;
-    public event EventHandler Aborted;
+    private readonly Func<TEdge, double> _weights;
 
     public event ClosenessResultAction<TVertex> VertexResult;
 
-    public ComputationState State { get; private set; } = ComputationState.NotRunning;
-
-    public object SyncRoot { get; } = new object();
+    private readonly object _syncRoot = new object();
 
     public Closeness(IVertexListGraph<TVertex, TEdge> graph, Func<TEdge, double> weights)
     {
@@ -30,81 +22,49 @@ namespace GraphML.Analysis.SNA.Centrality
       _weights = weights;
     }
 
-    public void Abort()
-    {
-      State = ComputationState.PendingAbortion;
-      StateChanged?.Invoke(this, new EventArgs());
-    }
-
     public void Compute()
     {
-      try
+      var toProcess = _graph.Vertices.Count();
+      if (toProcess == 0)
       {
-        State = ComputationState.Running;
-        Started?.Invoke(this, new EventArgs());
-        StateChanged?.Invoke(this, new EventArgs());
-
-        var toProcess = _graph.Vertices.Count();
-        if (toProcess == 0)
-        {
-          return;
-        }
-
-        using (var resetEvent = new ManualResetEvent(false))
-        {
-          foreach (var node in _graph.Vertices)
-          {
-            ThreadPool.QueueUserWorkItem(x =>
-            {
-              var algo = new DijkstraShortestPathAlgorithm<TVertex, TEdge>(_graph, _weights);
-              var distObserver = new VertexDistanceRecorderObserver<TVertex, TEdge>(_weights);
-              using (var distObserverDisp = distObserver.Attach(algo))
-              {
-                if (State == ComputationState.PendingAbortion)
-                {
-                  return;
-                }
-                algo.Compute(node);
-              }
-
-              var distances = distObserver.Distances.Sum(dist => dist.Value);
-              var closeness = (_graph.Vertices.Count() - 1) / distances;
-
-              lock (SyncRoot)
-              {
-                OnVertexResult(new ClosenessVertexResult<TVertex>(node, closeness));
-              }
-
-              // Safely decrement the counter
-              if (Interlocked.Decrement(ref toProcess) == 0)
-              {
-                resetEvent.Set();
-              }
-            });
-          }
-          resetEvent.WaitOne();
-        }
+        return;
       }
-      finally
+
+      using (var resetEvent = new ManualResetEvent(false))
       {
-        if (State == ComputationState.PendingAbortion)
+        foreach (var node in _graph.Vertices)
         {
-          State = ComputationState.Aborted;
-          Aborted?.Invoke(this, new EventArgs());
-          StateChanged?.Invoke(this, new EventArgs());
+          ThreadPool.QueueUserWorkItem(x =>
+          {
+            var algo = new DijkstraShortestPathAlgorithm<TVertex, TEdge>(_graph, _weights);
+            var distObserver = new VertexDistanceRecorderObserver<TVertex, TEdge>(_weights);
+            using (var distObserverDisp = distObserver.Attach(algo))
+            {
+              algo.Compute(node);
+            }
+
+            var distances = distObserver.Distances.Sum(dist => dist.Value);
+            var closeness = (_graph.Vertices.Count() - 1) / distances;
+
+            OnVertexResult(new ClosenessVertexResult<TVertex>(node, closeness));
+
+            // Safely decrement the counter
+            if (Interlocked.Decrement(ref toProcess) == 0)
+            {
+              resetEvent.Set();
+            }
+          });
         }
-        else
-        {
-          State = ComputationState.Finished;
-          Finished?.Invoke(this, new EventArgs());
-          StateChanged?.Invoke(this, new EventArgs());
-        }
+        resetEvent.WaitOne();
       }
     }
 
     private void OnVertexResult(ClosenessVertexResult<TVertex> result)
     {
-      VertexResult?.Invoke(result);
+      lock (_syncRoot)
+      {
+        VertexResult?.Invoke(result);
+      }
     }
   }
 }
