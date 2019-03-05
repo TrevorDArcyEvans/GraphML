d9b7976e724b29e789f7c055abbc0ea347c68a3c
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-03-05 10:49:34 +0000

    - db implementation of Closeness

--- GraphML.Analysis.SNA/Centrality/ClosenessJob.cs
+++ GraphML.Analysis.SNA/Centrality/ClosenessJob.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using QuickGraph;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GraphML.Analysis.SNA.Centrality
 {
@@ -36,115 +37,29 @@ namespace GraphML.Analysis.SNA.Centrality
     {
       var closeReq = (IClosenessRequest)req;
 
-      // TODO   retreive edges from db
-      // TODO   retreive nodes from db
-      #region Create the edges
-      var a_b = new Edge<string>("A", "B");
-      var a_d = new Edge<string>("A", "D");
-      var b_a = new Edge<string>("B", "A");
-      var b_c = new Edge<string>("B", "C");
-      var b_e = new Edge<string>("B", "E");
-      var c_b = new Edge<string>("C", "B");
-      var c_f = new Edge<string>("C", "F");
-      var c_j = new Edge<string>("C", "J");
-      var d_e = new Edge<string>("D", "E");
-      var d_g = new Edge<string>("D", "G");
-      var e_d = new Edge<string>("E", "D");
-      var e_f = new Edge<string>("E", "F");
-      var e_h = new Edge<string>("E", "H");
-      var f_i = new Edge<string>("F", "I");
-      var f_j = new Edge<string>("F", "J");
-      var g_d = new Edge<string>("G", "D");
-      var g_h = new Edge<string>("G", "H");
-      var h_g = new Edge<string>("H", "G");
-      var h_i = new Edge<string>("H", "I");
-      var i_f = new Edge<string>("I", "F");
-      var i_j = new Edge<string>("I", "J");
-      var i_h = new Edge<string>("I", "H");
-      var j_f = new Edge<string>("J", "F");
+      var graph = new AdjacencyGraph<string, IEdge<string>>();
 
-      var f_k = new Edge<string>("F", "K");
-      var k_i = new Edge<string>("K", "I");
-      #endregion
+      // raw nodes from db
+      var nodes = _nodeDatastore.ByOwners(new[] { closeReq.GraphId }, 1, int.MaxValue);
 
-      #region Define some weights to the edges
-      var edgeCost = new Dictionary<IEdge<string>, double>();
-      edgeCost.Add(a_b, 4);
-      edgeCost.Add(a_d, 1);
-      edgeCost.Add(b_a, 74);
-      edgeCost.Add(b_c, 2);
-      edgeCost.Add(b_e, 12);
-      edgeCost.Add(c_b, 12);
-      edgeCost.Add(c_f, 74);
-      edgeCost.Add(c_j, 12);
-      edgeCost.Add(d_e, 32);
-      edgeCost.Add(d_g, 22);
-      edgeCost.Add(e_d, 66);
-      edgeCost.Add(e_f, 76);
-      edgeCost.Add(e_h, 33);
-      edgeCost.Add(f_i, 11);
-      edgeCost.Add(f_j, 21);
-      edgeCost.Add(g_d, 12);
-      edgeCost.Add(g_h, 10);
-      edgeCost.Add(h_g, 2);
-      edgeCost.Add(h_i, 72);
-      edgeCost.Add(i_f, 31);
-      edgeCost.Add(i_h, 18);
-      edgeCost.Add(i_j, 7);
-      edgeCost.Add(j_f, 8);
+      // convert raw nodes to QuickGraph nodes
+      var qgNodes = nodes.Select(n => n.Id);
 
-      edgeCost.Add(f_k, 3);
-      edgeCost.Add(k_i, 8);
-      #endregion
+      // add nodes to graph
+      graph.AddVertexRange(qgNodes);
 
-      var _graph = new AdjacencyGraph<string, IEdge<string>>();
-      _graph.AddVertexRange(
-        new[]
-        {
-          "A",
-          "B",
-          "C",
-          "D",
-          "E",
-          "F",
-          "G",
-          "H",
-          "I",
-          "J",
-          "K"
-        });
-      _graph.AddEdgeRange(
-        new[]
-        {
-          a_b,
-          a_d,
-          b_a,
-          b_c,
-          b_e,
-          c_b,
-          c_f,
-          c_j,
-          d_e,
-          d_g,
-          e_d,
-          e_f,
-          e_h,
-          f_i,
-          f_j,
-          g_d,
-          g_h,
-          h_g,
-          h_i,
-          i_f,
-          i_j,
-          i_h,
-          j_f,
+      // raw edges from db
+      var edges = _edgeDatastore.ByOwners(new[] { closeReq.GraphId }, 1, int.MaxValue);
 
-          f_k,
-          k_i,
-        });
+      // convert raw edges to QuickGraph edges
+      var qgEdges = edges.Select(e => new Edge<string>(e.SourceId, e.TargetId));
 
-      var algo = _factory.Create(_graph, e => edgeCost[e]);
+      // add edges to graph
+      graph.AddEdgeRange(qgEdges);
+
+      // use unweighted edges
+      var algo = _factory.Create(graph, e => 1.0);
+
       var resultsList = new List<ClosenessVertexResult<string>>();
       algo.VertexResult += vertexRes => resultsList.Add(vertexRes);
 
