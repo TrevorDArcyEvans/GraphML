ab2148172d48c2ca7cd2469fc0323e483a19ba5c
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-05-08 09:04:41 +0100

    - make sure Import dialog resizes

--- GraphML.UI.Desktop/Import.Designer.cs
+++ GraphML.UI.Desktop/Import.Designer.cs
@@ -31,6 +31,7 @@
       System.Windows.Forms.Label label1;
       System.Windows.Forms.Label label2;
       System.Windows.Forms.Label label3;
+      System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
       this.CmdImport = new System.Windows.Forms.Button();
       this.TxtRepository = new System.Windows.Forms.TextBox();
       this.TxtDataFileName = new System.Windows.Forms.TextBox();
@@ -40,59 +41,76 @@
       label1 = new System.Windows.Forms.Label();
       label2 = new System.Windows.Forms.Label();
       label3 = new System.Windows.Forms.Label();
+      tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      tableLayoutPanel1.SuspendLayout();
       this.SuspendLayout();
-      // 
-      // CmdImport
-      // 
-      this.CmdImport.AutoSize = true;
-      this.CmdImport.Enabled = false;
-      this.CmdImport.Location = new System.Drawing.Point(310, 435);
-      this.CmdImport.Name = "CmdImport";
-      this.CmdImport.Size = new System.Drawing.Size(103, 30);
-      this.CmdImport.TabIndex = 0;
-      this.CmdImport.Text = "Import";
-      this.CmdImport.UseVisualStyleBackColor = true;
-      this.CmdImport.Click += new System.EventHandler(this.CmdImport_Click);
       // 
       // label1
       // 
       label1.AutoSize = true;
-      label1.Location = new System.Drawing.Point(13, 13);
+      label1.Location = new System.Drawing.Point(3, 0);
       label1.Name = "label1";
       label1.Size = new System.Drawing.Size(89, 20);
       label1.TabIndex = 1;
       label1.Text = "Repository:";
       // 
-      // TxtRepository
-      // 
-      this.TxtRepository.Location = new System.Drawing.Point(109, 6);
-      this.TxtRepository.Name = "TxtRepository";
-      this.TxtRepository.ReadOnly = true;
-      this.TxtRepository.Size = new System.Drawing.Size(304, 26);
-      this.TxtRepository.TabIndex = 2;
-      // 
       // label2
       // 
       label2.AutoSize = true;
-      label2.Location = new System.Drawing.Point(12, 55);
+      label2.Location = new System.Drawing.Point(3, 32);
       label2.Name = "label2";
       label2.Size = new System.Drawing.Size(77, 20);
       label2.TabIndex = 1;
       label2.Text = "Data File:";
       // 
+      // label3
+      // 
+      label3.AutoSize = true;
+      label3.Location = new System.Drawing.Point(3, 68);
+      label3.Name = "label3";
+      label3.Size = new System.Drawing.Size(40, 20);
+      label3.TabIndex = 1;
+      label3.Text = "Log:";
+      // 
+      // CmdImport
+      // 
+      this.CmdImport.AutoSize = true;
+      tableLayoutPanel1.SetColumnSpan(this.CmdImport, 2);
+      this.CmdImport.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.CmdImport.Enabled = false;
+      this.CmdImport.Location = new System.Drawing.Point(536, 516);
+      this.CmdImport.Name = "CmdImport";
+      this.CmdImport.Size = new System.Drawing.Size(161, 30);
+      this.CmdImport.TabIndex = 0;
+      this.CmdImport.Text = "Import";
+      this.CmdImport.UseVisualStyleBackColor = true;
+      this.CmdImport.Click += new System.EventHandler(this.CmdImport_Click);
+      // 
+      // TxtRepository
+      // 
+      tableLayoutPanel1.SetColumnSpan(this.TxtRepository, 3);
+      this.TxtRepository.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.TxtRepository.Location = new System.Drawing.Point(98, 3);
+      this.TxtRepository.Name = "TxtRepository";
+      this.TxtRepository.ReadOnly = true;
+      this.TxtRepository.Size = new System.Drawing.Size(599, 26);
+      this.TxtRepository.TabIndex = 2;
+      // 
       // TxtDataFileName
       // 
-      this.TxtDataFileName.Location = new System.Drawing.Point(109, 49);
+      tableLayoutPanel1.SetColumnSpan(this.TxtDataFileName, 2);
+      this.TxtDataFileName.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.TxtDataFileName.Location = new System.Drawing.Point(98, 35);
       this.TxtDataFileName.Name = "TxtDataFileName";
       this.TxtDataFileName.ReadOnly = true;
-      this.TxtDataFileName.Size = new System.Drawing.Size(254, 26);
+      this.TxtDataFileName.Size = new System.Drawing.Size(562, 26);
       this.TxtDataFileName.TabIndex = 2;
       // 
       // CmdBrowseDataFile
       // 
       this.CmdBrowseDataFile.AutoSize = true;
       this.CmdBrowseDataFile.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-      this.CmdBrowseDataFile.Location = new System.Drawing.Point(382, 45);
+      this.CmdBrowseDataFile.Location = new System.Drawing.Point(666, 35);
       this.CmdBrowseDataFile.Name = "CmdBrowseDataFile";
       this.CmdBrowseDataFile.Size = new System.Drawing.Size(31, 30);
       this.CmdBrowseDataFile.TabIndex = 3;
@@ -108,38 +126,55 @@
       // 
       // TxtLog
       // 
+      tableLayoutPanel1.SetColumnSpan(this.TxtLog, 3);
+      this.TxtLog.Dock = System.Windows.Forms.DockStyle.Fill;
       this.TxtLog.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.TxtLog.Location = new System.Drawing.Point(109, 103);
+      this.TxtLog.Location = new System.Drawing.Point(98, 71);
       this.TxtLog.Multiline = true;
       this.TxtLog.Name = "TxtLog";
       this.TxtLog.ReadOnly = true;
       this.TxtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-      this.TxtLog.Size = new System.Drawing.Size(304, 298);
+      this.TxtLog.Size = new System.Drawing.Size(599, 439);
       this.TxtLog.TabIndex = 2;
+      this.TxtLog.WordWrap = false;
       // 
-      // label3
+      // tableLayoutPanel1
       // 
-      label3.AutoSize = true;
-      label3.Location = new System.Drawing.Point(13, 103);
-      label3.Name = "label3";
-      label3.Size = new System.Drawing.Size(40, 20);
-      label3.TabIndex = 1;
-      label3.Text = "Log:";
+      tableLayoutPanel1.AutoSize = true;
+      tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+      tableLayoutPanel1.ColumnCount = 4;
+      tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+      tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+      tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+      tableLayoutPanel1.Controls.Add(this.CmdImport, 2, 3);
+      tableLayoutPanel1.Controls.Add(this.TxtLog, 1, 2);
+      tableLayoutPanel1.Controls.Add(this.CmdBrowseDataFile, 3, 1);
+      tableLayoutPanel1.Controls.Add(label3, 0, 2);
+      tableLayoutPanel1.Controls.Add(label2, 0, 1);
+      tableLayoutPanel1.Controls.Add(this.TxtDataFileName, 1, 1);
+      tableLayoutPanel1.Controls.Add(label1, 0, 0);
+      tableLayoutPanel1.Controls.Add(this.TxtRepository, 1, 0);
+      tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+      tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+      tableLayoutPanel1.Name = "tableLayoutPanel1";
+      tableLayoutPanel1.RowCount = 4;
+      tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+      tableLayoutPanel1.Size = new System.Drawing.Size(700, 549);
+      tableLayoutPanel1.TabIndex = 4;
       // 
       // Import
       // 
-      this.ClientSize = new System.Drawing.Size(459, 495);
-      this.Controls.Add(this.CmdBrowseDataFile);
-      this.Controls.Add(this.TxtLog);
-      this.Controls.Add(this.TxtDataFileName);
-      this.Controls.Add(this.TxtRepository);
-      this.Controls.Add(label3);
-      this.Controls.Add(label2);
-      this.Controls.Add(label1);
-      this.Controls.Add(this.CmdImport);
+      this.ClientSize = new System.Drawing.Size(700, 549);
+      this.Controls.Add(tableLayoutPanel1);
       this.Name = "Import";
       this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
       this.Text = "Import Data";
+      tableLayoutPanel1.ResumeLayout(false);
+      tableLayoutPanel1.PerformLayout();
       this.ResumeLayout(false);
       this.PerformLayout();
 
