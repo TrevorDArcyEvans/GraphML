77fa777f148c2393e3332721ed9948139647c904
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-05-16 15:35:24 +0100

    - refactored

---
+++ GraphML.UI.Web/Pages/BrowseGraphItem.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GraphML.UI.Web.Pages
+{
+  public class BrowseGraphItem : ComponentBase
+  {
+    [Inject]
+    private IUriHelper UriHelper { get; set; }
+
+    [Parameter]
+    protected string GraphName { get; private set; }
+
+    [Parameter]
+    protected string GraphId { get; private set; }
+
+    protected void BrowseGraph()
+    {
+      UriHelper.NavigateTo($"/BrowseGraph/{GraphName}/{GraphId}");
+    }
+  }
+}
