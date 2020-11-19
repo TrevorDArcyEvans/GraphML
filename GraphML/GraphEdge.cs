a1b1e49308057612c4b0b7585358de2a6f59f4f4
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-11-19 22:58:35 +0000

    - removed graphitem origin ambiguity

--- GraphML/GraphEdge.cs
+++ GraphML/GraphEdge.cs
@@ -1,6 +1,4 @@
-using Newtonsoft.Json;
 using System;
-using System.ComponentModel.DataAnnotations;
 using Schema = System.ComponentModel.DataAnnotations.Schema;
 
 namespace GraphML
@@ -8,24 +6,14 @@ namespace GraphML
   [Schema.Table(nameof(GraphEdge))]
   public sealed class GraphEdge : GraphItem
   {
-    [Required]
-    [JsonProperty(nameof(SourceId))]
-    public Guid SourceId { get; set; }
-
-    [Required]
-    [JsonProperty(nameof(TargetId))]
-    public Guid TargetId { get; set; }
-
     public GraphEdge() :
       base()
     {
     }
 
-    public GraphEdge(Guid graph,Guid repositoryItem,  string name, Guid source, Guid target) :
+    public GraphEdge(Guid graph, Guid repositoryItem,  string name) :
       base(graph, repositoryItem, name)
     {
-      SourceId = source;
-      TargetId = target;
     }
   }
 }
