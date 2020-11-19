359c007533be5fdc30140d797cd369c0f6f921d7
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-11-19 15:41:43 +0000

    - install custom handler for Guid stored as string for SQLite

---
+++ GraphML.API/GuidTypeHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using Dapper;
+
+namespace GraphML.API
+{
+	public sealed class GuidTypeHandler : SqlMapper.TypeHandler<Guid>
+	{
+		public override void SetValue(IDbDataParameter parameter, Guid guid)
+		{
+			parameter.Value = guid.ToString();
+		}
+
+		public override Guid Parse(object value)
+		{
+			return new Guid((string)value);
+		}
+	}
+}
