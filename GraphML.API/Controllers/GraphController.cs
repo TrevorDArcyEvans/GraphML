96a55d30769a04d6bacb045ad5e80a8027cbe308
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-11-20 19:16:30 +0000

    - formatting

--- GraphML.API/Controllers/GraphController.cs
+++ GraphML.API/Controllers/GraphController.cs
@@ -93,6 +93,7 @@ namespace GraphML.API.Controllers
     {
       return DeleteInternal(entity);
     }
+
     /// <summary>
     /// Update an existing Entities with new information
     /// </summary>
