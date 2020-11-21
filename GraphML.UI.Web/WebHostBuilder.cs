027c8a4f95aed2107818f0e7238f29042ba8e88b
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-11-21 18:25:59 +0000

    - do not use obsolete APIs

--- GraphML.UI.Web/WebHostBuilder.cs
+++ GraphML.UI.Web/WebHostBuilder.cs
@@ -1,36 +1,15 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace GraphML.UI.Web
 {
-	internal static class WebHostBuilder
+  internal static class WebHostBuilder
 	{
-		public static IHost BuildWebHost(string[] args)
+		public static IWebHost BuildWebHost(string[] args)
 		{
-			//var config = new ConfigurationBuilder()
-			//  .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-			//  .AddJsonFile("hosting.json")
-			//  .AddJsonFile($"autofac.json", optional: true)
-			//  .AddEnvironmentVariables()
-			//  .AddUserSecrets<Program>()
-			//  .Build();
-
-			return Host.CreateDefaultBuilder(args)
-    		  .ConfigureWebHostDefaults(webBuilder =>
-					  {
-						  webBuilder.UseStartup<Startup>();
-					  })
-			  //.UseKestrel()
-			  //.ConfigureServices(services => services.AddAutofac())
-			  //.UseConfiguration(config)
-			  //.ConfigureLogging(logging =>
-			  //{
-				 // logging.SetMinimumLevel(LogLevel.Trace);
-			  //})
-			  //.UseNLog()  // NLog: setup NLog for Dependency injection
+			return WebHost.CreateDefaultBuilder(args)
+    		.UseStartup<Startup>()
+			  .UseKestrel()
 			  .Build();
 		}
 	}
