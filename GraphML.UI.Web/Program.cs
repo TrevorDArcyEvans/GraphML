30c3b717825575d573843ea4d874844e9ff5b231
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-11-22 15:55:02 +0000

    - added support for NLog

--- GraphML.UI.Web/Program.cs
+++ GraphML.UI.Web/Program.cs
@@ -1,13 +1,22 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
 
 namespace GraphML.UI.Web
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			WebHostBuilder.BuildWebHost(args).Run();
-		}
-	}
+#pragma warning disable CS1591
+  public sealed class Program
+  {
+    public static void Main(string[] args)
+    {
+      try
+      {
+        WebHostBuilder.BuildWebHost(args).Run();
+      }
+      finally
+      {
+        // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+        NLog.LogManager.Shutdown();
+      }
+    }
+  }
+#pragma warning restore CS1591
 }
--- GraphML.UI.Web/WebHostBuilder.cs
+++ GraphML.UI.Web/WebHostBuilder.cs
@@ -1,16 +1,34 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace GraphML.UI.Web
 {
   internal static class WebHostBuilder
-	{
-		public static IWebHost BuildWebHost(string[] args)
-		{
-			return WebHost.CreateDefaultBuilder(args)
-    		.UseStartup<Startup>()
-			  .UseKestrel()
-			  .Build();
-		}
-	}
+  {
+    public static IWebHost BuildWebHost(string[] args)
+    {
+      var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile("hosting.json")
+        .AddEnvironmentVariables()
+        .AddUserSecrets<Program>()
+        .Build();
+
+      return WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>()
+        .UseKestrel()
+        .UseConfiguration(config)
+        .ConfigureLogging(logging =>
+        {
+          logging.SetMinimumLevel(LogLevel.Trace);
+        })
+        .UseNLog()  // NLog: setup NLog for Dependency injection
+        .Build();
+    }
+  }
 }
