e1bef8dcb55b8a3150d3ec3f4221a20a40fc4ecb
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-11-23 12:07:58 +0000

    - mirror GraphController

--- GraphML.API/Server/GraphServer.cs
+++ GraphML.API/Server/GraphServer.cs
@@ -1,17 +1,29 @@
 ﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 
 namespace GraphML.API.Server
 {
-  public sealed class GraphServer : OwnedItemServerBase<Graph>, IGraphServer
-  {
-    public GraphServer(
-      IRestClientFactory clientFactory,
-      ILogger<GraphServer> logger,
-      ISyncPolicyFactory policy) :
-      base(clientFactory, logger, policy)
-    {
-    }
+	public sealed class GraphServer : OwnedItemServerBase<Graph>, IGraphServer
+	{
+		public GraphServer(
+		  IRestClientFactory clientFactory,
+		  ILogger<GraphServer> logger,
+		  ISyncPolicyFactory policy) :
+		  base(clientFactory, logger, policy)
+		{
+		}
 
-    protected override string ResourceBase { get; } = "/api/Graph";
-  }
+		protected override string ResourceBase { get; } = $"/api/{nameof(Graph)}";
+
+		public IEnumerable<Graph> ByEdgeId(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Graph> ByNodeId(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
--- GraphML.API/Server/IGraphServer.cs
+++ GraphML.API/Server/IGraphServer.cs
@@ -1,6 +1,11 @@
-﻿namespace GraphML.API.Server
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphML.API.Server
 {
   public interface IGraphServer : IOwnedItemServerBase<Graph>
   {
+      IEnumerable<Graph> ByNodeId(Guid id);
+      IEnumerable<Graph> ByEdgeId(Guid id);
   }
 }
