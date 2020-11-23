55c592d38d9eb60a8255583817be3538b9493cd1
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-11-23 12:48:49 +0000

    - tie to AnalysisController

--- GraphML.API/Server/AnalysisServer.cs
+++ GraphML.API/Server/AnalysisServer.cs
@@ -3,51 +3,52 @@ using GraphML.Analysis.RankedShortestPath;
 using GraphML.Analysis.SNA.Centrality;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using GraphML.API.Controllers;
 
 namespace GraphML.API.Server
 {
-  public sealed class AnalysisServer : ServerBase, IAnalysisServer
-  {
-    public AnalysisServer(
-      IRestClientFactory clientFactory, 
-      ILogger<AnalysisServer> logger, 
-      ISyncPolicyFactory policy) : 
-      base(clientFactory, logger, policy)
-    {
-    }
+	public sealed class AnalysisServer : ServerBase, IAnalysisServer
+	{
+		public AnalysisServer(
+		  IRestClientFactory clientFactory,
+		  ILogger<AnalysisServer> logger,
+		  ISyncPolicyFactory policy) :
+		  base(clientFactory, logger, policy)
+		{
+		}
 
-    protected override string ResourceBase { get; } = "/api/Analysis";
+		protected override string ResourceBase { get; } = "/api/Analysis";
 
-    public async Task<string> Betweenness(BetweennessRequest req)
-    {
-      var request = GetRequest(Url.Combine(ResourceBase, "Betweenness"), req);
-      var retval = await GetResponse<string>(request);
+		public async Task<string> Betweenness(BetweennessRequest req)
+		{
+			var request = GetRequest(Url.Combine(ResourceBase, $"{nameof(AnalysisController.Betweenness)}"), req);
+			var retval = await GetResponse<string>(request);
 
-      return retval;
-    }
+			return retval;
+		}
 
-    public async Task<string> Closeness(ClosenessRequest req)
-    {
-      var request = GetRequest(Url.Combine(ResourceBase, "Closeness"), req);
-      var retval = await GetResponse<string>(request);
+		public async Task<string> Closeness(ClosenessRequest req)
+		{
+			var request = GetRequest(Url.Combine(ResourceBase, $"{nameof(AnalysisController.Closeness)}"), req);
+			var retval = await GetResponse<string>(request);
 
-      return retval;
-    }
+			return retval;
+		}
 
-    public async Task<string> Degree(DegreeRequest req)
-    {
-      var request = GetRequest(Url.Combine(ResourceBase, "Degree"), req);
-      var retval = await GetResponse<string>(request);
+		public async Task<string> Degree(DegreeRequest req)
+		{
+			var request = GetRequest(Url.Combine(ResourceBase, $"{nameof(AnalysisController.Degree)}"), req);
+			var retval = await GetResponse<string>(request);
 
-      return retval;
-    }
+			return retval;
+		}
 
-    public async Task<string> FindShortestPaths(FindShortestPathsRequest req)
-    {
-      var request = GetRequest(Url.Combine(ResourceBase, "FindShortestPaths"), req);
-      var retval = await GetResponse<string>(request);
+		public async Task<string> FindShortestPaths(FindShortestPathsRequest req)
+		{
+			var request = GetRequest(Url.Combine(ResourceBase, $"{nameof(AnalysisController.FindShortestPaths)}"), req);
+			var retval = await GetResponse<string>(request);
 
-      return retval;
-    }
-  }
-  }
+			return retval;
+		}
+	}
+}
