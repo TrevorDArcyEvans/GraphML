102ee3658bfaac3f67d03d6ab175b3307fd510d6
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-11-23 12:05:09 +0000

    - tie to ContactController

--- GraphML.API/Server/ContactServer.cs
+++ GraphML.API/Server/ContactServer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using System.Web;
 using Flurl;
+using GraphML.API.Controllers;
 using Microsoft.Extensions.Logging;
 
 namespace GraphML.API.Server
@@ -15,12 +16,12 @@ namespace GraphML.API.Server
     {
     }
 
-    protected override string ResourceBase { get; } = "/api/Contact";
+    protected override string ResourceBase { get; } = $"/api/{nameof(Contact)}";
 
     public async Task<Contact> ByEmail(string email)
     {
       var convertedEmail = HttpUtility.UrlDecode(email);
-      var request = GetRequest(Url.Combine(ResourceBase, "ByEmail", convertedEmail));
+      var request = GetRequest(Url.Combine(ResourceBase, $"{nameof(ContactController.ByEmail)}", convertedEmail));
       var retval = await GetResponse<Contact>(request);
 
       return retval;
