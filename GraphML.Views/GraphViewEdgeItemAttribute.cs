8d8739c7ae1470427de558c359fca2c4336a46bb
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-11-23 12:54:28 +0000

    - no longer using views
    -- support removed from db

--- GraphML.Views/GraphViewEdgeItemAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using Schema = System.ComponentModel.DataAnnotations.Schema;
-
-namespace GraphML.Views
-{
-  [Schema.Table(nameof(GraphViewEdgeItemAttribute))]
-  public sealed class GraphViewEdgeItemAttribute : GraphViewItemAttribute
-  {
-  }
-}
--- GraphML.Views/GraphViewNodeItemAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using Schema = System.ComponentModel.DataAnnotations.Schema;
-
-namespace GraphML.Views
-{
-  [Schema.Table(nameof(GraphViewNodeItemAttribute))]
-  public sealed class GraphViewNodeItemAttribute : GraphViewItemAttribute
-  {
-  }
-}
