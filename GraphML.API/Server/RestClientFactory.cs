b9af4663cda0bfbc711257bcebc8269af47e5620
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-11-23 17:08:23 +0000

    - code cleanup

--- GraphML.API/Server/RestClientFactory.cs
+++ GraphML.API/Server/RestClientFactory.cs
@@ -6,38 +6,38 @@ using System.Configuration;
 
 namespace GraphML.API.Server
 {
-  public sealed class RestClientFactory : IRestClientFactory
-  {
-    private readonly string ApiUri;
+	public sealed class RestClientFactory : IRestClientFactory
+	{
+		private readonly string ApiUri;
 
-    private readonly string UserName;
-    private readonly string Password;
+		private readonly string UserName;
+		private readonly string Password;
 
-    public RestClientFactory(IConfiguration config)
-    {
-      // read out of user secret or environment
-      ApiUri = Settings.API_URI(config);
-      UserName = Settings.API_USERNAME(config);
-      Password = Settings.API_PASSWORD(config);
+		public RestClientFactory(IConfiguration config)
+		{
+			// read out of user secret or environment
+			ApiUri = Settings.API_URI(config);
+			UserName = Settings.API_USERNAME(config);
+			Password = Settings.API_PASSWORD(config);
 
-      if (string.IsNullOrWhiteSpace(ApiUri) ||
-        string.IsNullOrWhiteSpace(UserName) ||
-        string.IsNullOrWhiteSpace(Password)
-        )
-      {
-        throw new ConfigurationErrorsException("Missing API configuration - check UserSecrets or environment variables");
-      }
-    }
+			if (string.IsNullOrWhiteSpace(ApiUri) ||
+			  string.IsNullOrWhiteSpace(UserName) ||
+			  string.IsNullOrWhiteSpace(Password)
+			  )
+			{
+				throw new ConfigurationErrorsException("Missing API configuration - check UserSecrets or environment variables");
+			}
+		}
 
-    public IRestClient GetRestClient()
-    {
-      var client = new RestClient(ApiUri)
-      {
-        Authenticator = new HttpBasicAuthenticator(UserName, Password)
-      };
-      client.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+		public IRestClient GetRestClient()
+		{
+			var client = new RestClient(ApiUri)
+			{
+				Authenticator = new HttpBasicAuthenticator(UserName, Password),
+				RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+			};
 
-      return client;
-    }
-  }
+			return client;
+		}
+	}
 }
