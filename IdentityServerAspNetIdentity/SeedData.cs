cc8b6132760c128babbede89ed6fb23af0bc6a70
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-12-21 19:32:57 +0000

    - Alice is DrKool

--- IdentityServerAspNetIdentity/SeedData.cs
+++ IdentityServerAspNetIdentity/SeedData.cs
@@ -53,7 +53,7 @@ namespace IdentityServerAspNetIdentity
                         new Claim(JwtClaimTypes.Name, "Alice Smith"),
                         new Claim(JwtClaimTypes.GivenName, "Alice"),
                         new Claim(JwtClaimTypes.FamilyName, "Smith"),
-                        new Claim(JwtClaimTypes.Email, "AliceSmith@email.com"),
+                        new Claim(JwtClaimTypes.Email, "DrKool@KoolOrganisation.org"),
                         new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
                         new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
                         new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json),
