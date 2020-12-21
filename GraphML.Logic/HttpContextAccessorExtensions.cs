ff2a33001b2fbfa40942004fd522a4a61728415e
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-12-21 14:51:14 +0000

    - use Jwt claim types

--- GraphML.Logic/HttpContextAccessorExtensions.cs
+++ GraphML.Logic/HttpContextAccessorExtensions.cs
@@ -1,34 +1,34 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System.Linq;
 using System.Security.Claims;
+using IdentityModel;
 
 namespace GraphML.Logic
 {
-  public static class HttpContextAccessorExtensions
-  {
-    public static string Email(this IHttpContextAccessor context)
-    {
-      return context.HttpContext.Email();
-    }
+	public static class HttpContextAccessorExtensions
+	{
+		public static string Email(this IHttpContextAccessor context)
+		{
+			return context.HttpContext.Email();
+		}
 
-    public static string Email(this HttpContext context)
-    {
-      // ClaimTypes.Email --> 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'
-      // but some OIDC providers use 'email'
-      return context.User.Claims
-        .FirstOrDefault(x =>
-          x.Type == ClaimTypes.Email ||
-          x.Type.ToLowerInvariant() == "email")?.Value;
-    }
+		public static string Email(this HttpContext context)
+		{
+			// ClaimTypes.Email --> 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'
+			// JwtClaimTypes.Email --> email'
+			return context.User.Claims
+			  .FirstOrDefault(x => x.Type == ClaimTypes.Email || x.Type == JwtClaimTypes.Email)
+        ?.Value;
+		}
 
-    public static bool HasRole(this IHttpContextAccessor context, string role)
-    {
-      // ClaimTypes.Role --> 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/role'
-      // but some OIDC providers use 'role'
-      return context.HttpContext.User.Claims
-        .Where(x => x.Type == ClaimTypes.Role || x.Type.ToLowerInvariant() == "role")
-        .Select(x => x.Value)
-        .Any(x => x == role);
-    }
-  }
+		public static bool HasRole(this IHttpContextAccessor context, string role)
+		{
+			// ClaimTypes.Role --> 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/role'
+			// JwtClaimTypes.Role --> 'role'
+			return context.HttpContext.User.Claims
+			    .Where(x => x.Type == ClaimTypes.Role || x.Type == JwtClaimTypes.Role)
+			    .Select(x => x.Value)
+			    .Any(x => x == role);
+		}
+	}
 }
