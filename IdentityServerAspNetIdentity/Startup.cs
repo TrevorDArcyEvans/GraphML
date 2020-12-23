3cae8dabd4c069dd5015ff3eb2d3f849c8ee6d47
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-12-23 15:44:58 +0000

    - formatting

--- IdentityServerAspNetIdentity/Startup.cs
+++ IdentityServerAspNetIdentity/Startup.cs
@@ -14,20 +14,20 @@ using Microsoft.Extensions.Hosting;
 
 namespace IdentityServerAspNetIdentity
 {
-    public class Startup
-    {
-        public IWebHostEnvironment Environment { get; }
-        public IConfiguration Configuration { get; }
+	public class Startup
+	{
+		public IWebHostEnvironment Environment { get; }
+		public IConfiguration Configuration { get; }
 
-        public Startup(IWebHostEnvironment environment, IConfiguration configuration)
-        {
-            Environment = environment;
-            Configuration = configuration;
-        }
+		public Startup(IWebHostEnvironment environment, IConfiguration configuration)
+		{
+			Environment = environment;
+			Configuration = configuration;
+		}
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllersWithViews();
 
 			services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
@@ -37,38 +37,38 @@ namespace IdentityServerAspNetIdentity
 				.AddDefaultTokenProviders();
 
 			var builder = services.AddIdentityServer(options =>
-	            {
-		            options.Events.RaiseErrorEvents = true;
-		            options.Events.RaiseInformationEvents = true;
-		            options.Events.RaiseFailureEvents = true;
-		            options.Events.RaiseSuccessEvents = true;
-	            })
-	            .AddInMemoryIdentityResources(Config.Ids)
-	            .AddInMemoryApiResources(Config.Apis)
-	            .AddInMemoryClients(Config.Clients)
+				{
+					options.Events.RaiseErrorEvents = true;
+					options.Events.RaiseInformationEvents = true;
+					options.Events.RaiseFailureEvents = true;
+					options.Events.RaiseSuccessEvents = true;
+				})
+				.AddInMemoryIdentityResources(Config.Ids)
+				.AddInMemoryApiResources(Config.Apis)
+				.AddInMemoryClients(Config.Clients)
 				.AddAspNetIdentity<ApplicationUser>();
 
 			// not recommended for production - you need to store your key material somewhere secure
 			builder.AddDeveloperSigningCredential();
-        }
+		}
 
-        public void Configure(IApplicationBuilder app)
-        {
-            if (Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
+		public void Configure(IApplicationBuilder app)
+		{
+			if (Environment.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
 
-            app.UseStaticFiles();
+			app.UseStaticFiles();
 
-            app.UseRouting();
-            app.UseIdentityServer();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-            });
-        }
-    }
+			app.UseRouting();
+			app.UseIdentityServer();
+			app.UseAuthorization();
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapDefaultControllerRoute();
+			});
+		}
+	}
 }
