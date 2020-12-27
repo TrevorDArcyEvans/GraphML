0d0d2ab812f4cdf7db0f91d49a341e6a05f2cfd7
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-12-27 21:05:05 +0000

    - added test for MustBeInSameGraph

--- GraphML.Logic.Tests/Validators/GraphNodesRequestValidator_Tests.cs
+++ GraphML.Logic.Tests/Validators/GraphNodesRequestValidator_Tests.cs
@@ -11,59 +11,81 @@ using NUnit.Framework;
 namespace GraphML.Logic.Tests.Validators
 {
   [TestFixture]
-	public sealed class GraphNodesRequestValidator_Tests
-	{
-		private Mock<IHttpContextAccessor> _context;
-		private Mock<IContactDatastore> _contactDatastore;
-		private Mock<IRoleDatastore> _roleDatastore;
-		private Mock<IGraphDatastore> _graphDatastore;
-		private Mock<IGraphNodeDatastore> _graphNodeDatastore;
+  public sealed class GraphNodesRequestValidator_Tests
+  {
+    private Mock<IHttpContextAccessor> _context;
+    private Mock<IContactDatastore> _contactDatastore;
+    private Mock<IRoleDatastore> _roleDatastore;
+    private Mock<IGraphDatastore> _graphDatastore;
+    private Mock<IGraphNodeDatastore> _graphNodeDatastore;
 
-		[SetUp]
-		public void Setup()
-		{
-			_context = new Mock<IHttpContextAccessor>();
-			_contactDatastore = new Mock<IContactDatastore>();
-			_roleDatastore = new Mock<IRoleDatastore>();
-			_graphDatastore = new Mock<IGraphDatastore>();
-			_graphNodeDatastore = new Mock<IGraphNodeDatastore>();
-		}
+    [SetUp]
+    public void Setup()
+    {
+      _context = new Mock<IHttpContextAccessor>();
+      _contactDatastore = new Mock<IContactDatastore>();
+      _roleDatastore = new Mock<IRoleDatastore>();
+      _graphDatastore = new Mock<IGraphDatastore>();
+      _graphNodeDatastore = new Mock<IGraphNodeDatastore>();
+    }
 
-		[Test]
-		public void RequesterMustBeSameOrganisation_SameOrganisation_Succeeds()
-		{
-			const string email = "DrStrangelove@USAF.com";
+    [Test]
+    public void RequesterMustBeSameOrganisation_SameOrganisation_Succeeds()
+    {
+      const string email = "DrStrangelove@USAF.com";
 
-			var org = new Organisation();
-			var graph = new Graph { OrganisationId = org.Id };
-			var graphNodeRoot = new GraphNode();
-			var graphNodeTarget = new GraphNode();
-			var graphNodes = new[] { graphNodeRoot, graphNodeTarget };
-			var contact = new Contact { OrganisationId = org.Id };
-			_context.Setup(x => x.HttpContext).Returns(Creator.GetContext(email));
-			_contactDatastore.Setup(x => x.ByEmail(email)).Returns(contact);
-			_graphNodeDatastore.Setup(x => x.ByIds(new[] { graphNodeRoot.Id, graphNodeTarget.Id })).Returns(graphNodes);
-			_graphDatastore.Setup(x => 
-          x.ByIds(It.Is<IEnumerable<Guid>>(gns => 
+      var org = new Organisation();
+      var graph = new Graph { OrganisationId = org.Id };
+      var graphNodeRoot = new GraphNode();
+      var graphNodeTarget = new GraphNode();
+      var graphNodes = new[] { graphNodeRoot, graphNodeTarget };
+      var contact = new Contact { OrganisationId = org.Id };
+      _context.Setup(x => x.HttpContext).Returns(Creator.GetContext(email));
+      _contactDatastore.Setup(x => x.ByEmail(email)).Returns(contact);
+      _graphNodeDatastore.Setup(x => x.ByIds(new[] { graphNodeRoot.Id, graphNodeTarget.Id })).Returns(graphNodes);
+      _graphDatastore.Setup(x =>
+          x.ByIds(It.Is<IEnumerable<Guid>>(gns =>
               gns.First() == graphNodeRoot.GraphId && gns.Last() == graphNodeTarget.GraphId)))
-          .Returns(new[] { graph });
-			var req = new DummyGraphNodesRequest { GraphNodes = new[] { graphNodeRoot.Id, graphNodeTarget.Id } };
-			var validator = Create();
+          .Returns(new[] { graph, graph });
+      var req = new DummyGraphNodesRequest { GraphNodes = new[] { graphNodeRoot.Id, graphNodeTarget.Id } };
+      var validator = Create();
 
-			validator.RequesterMustBeSameOrganisation();
-			var valres = validator.Validate(req);
+      validator.RequesterMustBeSameOrganisation();
+      var valres = validator.Validate(req);
 
-			valres.Errors.Should().BeEmpty();
-		}
+      valres.Errors.Should().BeEmpty();
+    }
 
-		private GraphNodesRequestValidator Create()
-		{
-			return new GraphNodesRequestValidator(
-				_context.Object,
-				_contactDatastore.Object,
-				_roleDatastore.Object,
-				_graphDatastore.Object,
-			  _graphNodeDatastore.Object);
-		}
-	}
+    [Test]
+    public void MustBeInSameGraph_SameGraph_Succeeds()
+    {
+      var org = new Organisation();
+      var graph = new Graph { OrganisationId = org.Id };
+      var graphNodeRoot = new GraphNode { GraphId = graph.Id };
+      var graphNodeTarget = new GraphNode { GraphId = graph.Id };
+      var graphNodes = new[] { graphNodeRoot, graphNodeTarget };
+      _graphNodeDatastore.Setup(x => x.ByIds(new[] { graphNodeRoot.Id, graphNodeTarget.Id })).Returns(graphNodes);
+      _graphDatastore.Setup(x =>
+          x.ByIds(It.Is<IEnumerable<Guid>>(gns =>
+              gns.First() == graphNodeRoot.GraphId && gns.Last() == graphNodeTarget.GraphId)))
+          .Returns(new[] { graph, graph });
+      var req = new DummyGraphNodesRequest { GraphNodes = new[] { graphNodeRoot.Id, graphNodeTarget.Id } };
+      var validator = Create();
+
+      validator.MustBeInSameGraph();
+      var valres = validator.Validate(req);
+
+      valres.Errors.Should().BeEmpty();
+    }
+
+    private GraphNodesRequestValidator Create()
+    {
+      return new GraphNodesRequestValidator(
+        _context.Object,
+        _contactDatastore.Object,
+        _roleDatastore.Object,
+        _graphDatastore.Object,
+        _graphNodeDatastore.Object);
+    }
+  }
 }
