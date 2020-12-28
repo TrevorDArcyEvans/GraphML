1df2d8091e31df475a3248fc29c7cba99e2c3d76
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-12-28 18:01:57 +0000

    - now using IdentityServerAspNetIdentity

--- IdentityServer/createCert.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-openssl x509 -req -days 365 -extfile https.config -extensions v3_req -in csr.pem -signkey key.pem -out https.crt
-openssl pkcs12 -export -out https.pfx -inkey key.pem -in https.crt -password pass:password
