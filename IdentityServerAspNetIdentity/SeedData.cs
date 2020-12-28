ef31d7e182e1c3af2cf5c8afc9a34504244efe7b
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2020-12-28 17:33:31 +0000

    - regioning

--- IdentityServerAspNetIdentity/SeedData.cs
+++ IdentityServerAspNetIdentity/SeedData.cs
@@ -15,41 +15,43 @@ using Serilog;
 
 namespace IdentityServerAspNetIdentity
 {
-    public class SeedData
+  public class SeedData
+  {
+    public static void EnsureSeedData(string connectionString)
     {
-        public static void EnsureSeedData(string connectionString)
+      var services = new ServiceCollection();
+      services.AddLogging();
+      services.AddDbContext<ApplicationDbContext>(options =>
+         options.UseSqlite(connectionString));
+
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+          .AddEntityFrameworkStores<ApplicationDbContext>()
+          .AddDefaultTokenProviders();
+
+      using (var serviceProvider = services.BuildServiceProvider())
+      {
+        using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
         {
-            var services = new ServiceCollection();
-            services.AddLogging();
-            services.AddDbContext<ApplicationDbContext>(options =>
-               options.UseSqlite(connectionString));
+          var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+          context.Database.Migrate();
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+          var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-            using (var serviceProvider = services.BuildServiceProvider())
+          #region Alice
+          var alice = userMgr.FindByNameAsync("alice").Result;
+          if (alice == null)
+          {
+            alice = new ApplicationUser
             {
-                using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
-                {
-                    var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
-                    context.Database.Migrate();
+              UserName = "alice"
+            };
+            var result = userMgr.CreateAsync(alice, "Pass123$").Result;
+            if (!result.Succeeded)
+            {
+              throw new Exception(result.Errors.First().Description);
+            }
 
-                    var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-                    var alice = userMgr.FindByNameAsync("alice").Result;
-                    if (alice == null)
-                    {
-                        alice = new ApplicationUser
-                        {
-                            UserName = "alice"
-                        };
-                        var result = userMgr.CreateAsync(alice, "Pass123$").Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
-
-                        result = userMgr.AddClaimsAsync(alice, new Claim[]{
+            result = userMgr.AddClaimsAsync(alice, new Claim[]{
                         new Claim(JwtClaimTypes.Name, "Alice Smith"),
                         new Claim(JwtClaimTypes.GivenName, "Alice"),
                         new Claim(JwtClaimTypes.FamilyName, "Smith"),
@@ -60,31 +62,33 @@ namespace IdentityServerAspNetIdentity
                         // Add user_scope claim for Identity to authorize UI and API actions. Wendy has this permission, Bob does not.
                         new Claim("appuser_claim","identity")
                         }).Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
-                        Log.Debug("alice created");
-                    }
-                    else
-                    {
-                        Log.Debug("alice already exists");
-                    }
+            if (!result.Succeeded)
+            {
+              throw new Exception(result.Errors.First().Description);
+            }
+            Log.Debug("alice created");
+          }
+          else
+          {
+            Log.Debug("alice already exists");
+          }
+          #endregion
 
-                    var bob = userMgr.FindByNameAsync("bob").Result;
-                    if (bob == null)
-                    {
-                        bob = new ApplicationUser
-                        {
-                            UserName = "bob"
-                        };
-                        var result = userMgr.CreateAsync(bob, "Pass123$").Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
+          #region Bob
+          var bob = userMgr.FindByNameAsync("bob").Result;
+          if (bob == null)
+          {
+            bob = new ApplicationUser
+            {
+              UserName = "bob"
+            };
+            var result = userMgr.CreateAsync(bob, "Pass123$").Result;
+            if (!result.Succeeded)
+            {
+              throw new Exception(result.Errors.First().Description);
+            }
 
-                        result = userMgr.AddClaimsAsync(bob, new Claim[]{
+            result = userMgr.AddClaimsAsync(bob, new Claim[]{
                         new Claim(JwtClaimTypes.Name, "Bob Smith"),
                         new Claim(JwtClaimTypes.GivenName, "Bob"),
                         new Claim(JwtClaimTypes.FamilyName, "Smith"),
@@ -94,18 +98,19 @@ namespace IdentityServerAspNetIdentity
                         new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json),
                         new Claim("location", "somewhere")
                     }).Result;
-                        if (!result.Succeeded)
-                        {
-                            throw new Exception(result.Errors.First().Description);
-                        }
-                        Log.Debug("bob created");
-                    }
-                    else
-                    {
-                        Log.Debug("bob already exists");
-                    }
-                }
+            if (!result.Succeeded)
+            {
+              throw new Exception(result.Errors.First().Description);
             }
+            Log.Debug("bob created");
+          }
+          else
+          {
+            Log.Debug("bob already exists");
+          }
+          #endregion
         }
+      }
     }
+  }
 }
