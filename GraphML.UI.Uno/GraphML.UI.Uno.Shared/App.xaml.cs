b92bcc9ce6f2c9a8020e5cd4812bff3140240d5c
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-03-10 15:19:21 +0000

    - support for developing on Windows UWP

--- GraphML.UI.Uno/GraphML.UI.Uno.Shared/App.xaml.cs
+++ GraphML.UI.Uno/GraphML.UI.Uno.Shared/App.xaml.cs
@@ -1,4 +1,4 @@
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -18,10 +18,10 @@ using Windows.UI.Xaml.Navigation;
 
 namespace GraphML.UI.Uno
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    public sealed partial class App : Application
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	public sealed partial class App : Application
 	{
 		/// <summary>
 		/// Initializes the singleton application object.  This is the first line of authored code
@@ -42,10 +42,21 @@ namespace GraphML.UI.Uno
 		/// <param name="e">Details about the launch request and process.</param>
 		protected override void OnLaunched(LaunchActivatedEventArgs e)
 		{
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
 
+#if NET5_0 && WINDOWS
+            var window = new Window();
+            window.Activate();
+#else
 			var window = Windows.UI.Xaml.Window.Current;
+#endif
 
-			Frame rootFrame = window.Content as Frame;
+			var rootFrame = window.Content as Frame;
 
 			// Do not repeat app initialization when the Window already has content,
 			// just ensure that the window is active
@@ -65,7 +76,9 @@ namespace GraphML.UI.Uno
 				window.Content = rootFrame;
 			}
 
+#if !(NET5_0 && WINDOWS)
 			if (e.PrelaunchActivated == false)
+#endif
 			{
 				if (rootFrame.Content == null)
 				{
@@ -105,9 +118,9 @@ namespace GraphML.UI.Uno
 
 
 		/// <summary>
-        /// Configures global logging
-        /// </summary>
-        /// <param name="factory"></param>
+		/// Configures global logging
+		/// </summary>
+		/// <param name="factory"></param>
 		static void ConfigureFilters(ILoggerFactory factory)
 		{
 			factory
@@ -116,41 +129,45 @@ namespace GraphML.UI.Uno
 						{ "Uno", LogLevel.Warning },
 						{ "Windows", LogLevel.Warning },
 
-						// Debug JS interop
-						// { "Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug },
+                        // Debug JS interop
+                        // { "Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug },
 
-						// Generic Xaml events
-						// { "Windows.UI.Xaml", LogLevel.Debug },
-						// { "Windows.UI.Xaml.VisualStateGroup", LogLevel.Debug },
-						// { "Windows.UI.Xaml.StateTriggerBase", LogLevel.Debug },
-						// { "Windows.UI.Xaml.UIElement", LogLevel.Debug },
+                        // Generic Xaml events
+                        // { "Windows.UI.Xaml", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.VisualStateGroup", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.StateTriggerBase", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.UIElement", LogLevel.Debug },
 
-						// Layouter specific messages
-						// { "Windows.UI.Xaml.Controls", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.Panel", LogLevel.Debug },
-						// { "Windows.Storage", LogLevel.Debug },
+                        // Layouter specific messages
+                        // { "Windows.UI.Xaml.Controls", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.Controls.Panel", LogLevel.Debug },
+                        // { "Windows.Storage", LogLevel.Debug },
 
-						// Binding related messages
-						// { "Windows.UI.Xaml.Data", LogLevel.Debug },
+                        // Binding related messages
+                        // { "Windows.UI.Xaml.Data", LogLevel.Debug },
 
-						// DependencyObject memory references tracking
-						// { "ReferenceHolder", LogLevel.Debug },
+                        // DependencyObject memory references tracking
+                        // { "ReferenceHolder", LogLevel.Debug },
 
-						// ListView-related messages
-						// { "Windows.UI.Xaml.Controls.ListViewBase", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.ListView", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.GridView", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.VirtualizingPanelLayout", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.NativeListViewBase", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.ListViewBaseSource", LogLevel.Debug }, //iOS
-						// { "Windows.UI.Xaml.Controls.ListViewBaseInternalContainer", LogLevel.Debug }, //iOS
-						// { "Windows.UI.Xaml.Controls.NativeListViewBaseAdapter", LogLevel.Debug }, //Android
-						// { "Windows.UI.Xaml.Controls.BufferViewCache", LogLevel.Debug }, //Android
-						// { "Windows.UI.Xaml.Controls.VirtualizingPanelGenerator", LogLevel.Debug }, //WASM
-					}
+                        // ListView-related messages
+                        // { "Windows.UI.Xaml.Controls.ListViewBase", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.Controls.ListView", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.Controls.GridView", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.Controls.VirtualizingPanelLayout", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.Controls.NativeListViewBase", LogLevel.Debug },
+                        // { "Windows.UI.Xaml.Controls.ListViewBaseSource", LogLevel.Debug }, //iOS
+                        // { "Windows.UI.Xaml.Controls.ListViewBaseInternalContainer", LogLevel.Debug }, //iOS
+                        // { "Windows.UI.Xaml.Controls.NativeListViewBaseAdapter", LogLevel.Debug }, //Android
+                        // { "Windows.UI.Xaml.Controls.BufferViewCache", LogLevel.Debug }, //Android
+                        // { "Windows.UI.Xaml.Controls.VirtualizingPanelGenerator", LogLevel.Debug }, //WASM
+                    }
 				)
+#if DEBUG
+                .AddConsole(LogLevel.Debug);
+#else
 				.AddConsole(LogLevel.Information);
+#endif
 		}
-  }
+	}
 }
