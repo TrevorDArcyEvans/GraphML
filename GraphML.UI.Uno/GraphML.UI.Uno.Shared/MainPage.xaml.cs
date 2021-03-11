9916bc10f4bde2e78fadb9881f9e6c3c61fde74d
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-03-11 12:50:05 +0000

    - walking skeleton of GraphML.UI.Uno username/password login

--- GraphML.UI.Uno/GraphML.UI.Uno.Shared/MainPage.xaml.cs
+++ GraphML.UI.Uno/GraphML.UI.Uno.Shared/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace GraphML.UI.Uno
+﻿using System.Net.Http;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace GraphML.UI.Uno
 {
 	using System;
 	using Windows.UI.Xaml.Controls;
@@ -12,10 +16,45 @@
 			dp.Date = new DateTime(2006, 2, 20);
 		}
 
-		private void OnClick(object sender, object args)
+		private async void OnClick(object sender, object args)
 		{
 			var dt = DateTime.Now.ToString("O");
 			txt.Text = dt;
+
+			var innerHandler = new HttpClientHandler
+			{
+				ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+			};
+			var client = new HttpClient(innerHandler)
+			{
+				BaseAddress = new Uri("https://localhost:44387")
+			};
+			var disco = await client.GetDiscoveryDocumentAsync();
+			if (disco.IsError)
+			{
+				throw new Exception(disco.Error);
+			}
+
+			var response = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+			{
+				Address = disco.TokenEndpoint,
+
+				ClientId = "GraphML.UI.Uno",
+				ClientSecret = "secret",
+
+				UserName = "alice",
+				Password = "Pass123$"
+			});
+
+			var api = new HttpClient(innerHandler)
+			{
+				BaseAddress = new Uri("https://localhost:5001/")
+			};
+			var token = response.AccessToken;
+			api.SetBearerToken(token);
+			var orgsResp = await api.GetAsync("api/Organisation/GetAll");
+			var orgsCont = await orgsResp.Content.ReadAsStringAsync();
+			var orgs = JArray.Parse(orgsCont);
 		}
 
 		public void Menu_OnClick(object sender, object args)
