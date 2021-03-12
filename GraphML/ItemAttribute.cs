20040f176019f17d78352779cca0ad81c6b5620b
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-03-12 17:33:48 +0000

    - updated for property name
    - added reference to ItemAttributeDefinition

--- GraphML/ItemAttribute.cs
+++ GraphML/ItemAttribute.cs
@@ -1,27 +1,35 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GraphML
 {
-  /// <summary>
-  /// Additional data information attached to an <see cref="Item"/>
-  /// </summary>
-  public abstract class ItemAttribute : OwnedItem
-  {
-    /// <summary>
-    /// Data value, serialised as a JSON string
-    /// </summary>
-    [JsonProperty(nameof(DataValueAsString))]
-    public string DataValueAsString { get; set; } = string.Empty;
+	/// <summary>
+	/// Additional data information attached to an <see cref="Item"/>
+	/// </summary>
+	public abstract class ItemAttribute : OwnedItem
+	{
+		/// <summary>
+		/// Unique identifier of <see cref="ItemAttributeDefinition"/>
+		/// </summary>
+		[Required]
+		[JsonProperty(nameof(DefinitionId))]
+		public Guid DefinitionId { get; set; } = Guid.Empty;
 
-    public ItemAttribute() :
-      base()
-    {
-    }
+		/// <summary>
+		/// Data value, serialised as a JSON string
+		/// </summary>
+		[JsonProperty(nameof(DataValueAsString))]
+		public string DataValueAsString { get; set; } = string.Empty;
 
-    public ItemAttribute(Guid owner, Guid orgId, string name) :
-      base(owner, orgId, name)
-    {
-    }
-  }
+		public ItemAttribute() :
+		  base()
+		{
+		}
+
+		public ItemAttribute(Guid owner, Guid orgId, string name) :
+		  base(owner, orgId, name)
+		{
+		}
+	}
 }
