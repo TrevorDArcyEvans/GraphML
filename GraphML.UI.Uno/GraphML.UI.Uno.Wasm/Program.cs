af1d25738a94782963dfd0cab3b6572d7fb1a115
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-03-15 17:30:32 +0000

    - code cleanup

--- GraphML.UI.Uno/GraphML.UI.Uno.Wasm/Program.cs
+++ GraphML.UI.Uno/GraphML.UI.Uno.Wasm/Program.cs
@@ -1,16 +1,16 @@
 namespace GraphML.UI.Uno.Wasm
 {
-  using Windows.UI.Xaml;
+	using Windows.UI.Xaml;
 
-  public class Program
-  {
-    private static App _app;
+	public class Program
+	{
+		private static App _app;
 
-    static int Main(string[] args)
-    {
-      Application.Start(_ => _app = new App());
+		public static int Main(string[] args)
+		{
+			Application.Start(_ => _app = new App());
 
-      return 0;
-    }
-  }
+			return 0;
+		}
+	}
 }
