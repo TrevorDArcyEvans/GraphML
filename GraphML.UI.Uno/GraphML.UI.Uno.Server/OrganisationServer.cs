d144f3784a947ecb99194c6d6f398bf60ed24b0f
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-03-17 19:39:50 +0000

    - create GraphML.UI.Uno.Server project to deal with data from server

---
+++ GraphML.UI.Uno/GraphML.UI.Uno.Server/OrganisationServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphML.Interfaces.Server;
+
+namespace GraphML.UI.Uno.Server
+{
+    public class OrganisationServer : IOrganisationServer
+    {
+        public Task<IEnumerable<Organisation>> ByIds(IEnumerable<Guid> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Organisation>> Create(IEnumerable<Organisation> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Organisation>> Delete(IEnumerable<Organisation> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Threading.Tasks.Task<IEnumerable<Organisation>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Organisation>> Update(IEnumerable<Organisation> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
