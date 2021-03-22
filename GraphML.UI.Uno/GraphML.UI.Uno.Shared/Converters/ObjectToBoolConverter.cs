8d81ec9cd427b574b49915f65eda0b1f45ea0c49
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-03-22 18:28:55 +0000

    - true if object is non-null

---
+++ GraphML.UI.Uno/GraphML.UI.Uno.Shared/Converters/ObjectToBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace GraphML.UI.Uno.Converters
+{
+	using System;
+	using Windows.UI.Xaml.Data;
+
+	public sealed class ObjectToBoolConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			return !(value is null);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
