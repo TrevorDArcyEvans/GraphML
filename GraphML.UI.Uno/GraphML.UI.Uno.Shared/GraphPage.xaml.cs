9ffd50c860cf03dd7e47261bf96de5e0e2a83d5a
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-03-22 20:57:11 +0000

    - code cleanup

--- GraphML.UI.Uno/GraphML.UI.Uno.Shared/GraphPage.xaml.cs
+++ GraphML.UI.Uno/GraphML.UI.Uno.Shared/GraphPage.xaml.cs
@@ -1,55 +1,54 @@
-﻿using System.Collections.ObjectModel;
-using GraphML.UI.Uno.Server;
-
-namespace GraphML.UI.Uno
+﻿namespace GraphML.UI.Uno
 {
-	using System.Linq;
-	using Windows.UI.Xaml.Navigation;
+  using GraphML.UI.Uno.Server;
+  using System.Collections.ObjectModel;
+  using System.Linq;
+  using Windows.UI.Xaml.Navigation;
 
-	public sealed partial class GraphPage : PageBase
-	{
-		private Graph _selectedGraph;
+  public sealed partial class GraphPage : PageBase
+  {
+    private Graph _selectedGraph;
 
-		public GraphPage() :
-			  base()
-		{
-			InitializeComponent();
-		}
+    public GraphPage() :
+        base()
+    {
+      InitializeComponent();
+    }
 
-		public ObservableCollection<Graph> Graphs { get; set; } = new ObservableCollection<Graph>();
+    public ObservableCollection<Graph> Graphs { get; set; } = new ObservableCollection<Graph>();
 
-		public Graph SelectedGraph
-		{
-			get => _selectedGraph;
-			set => SetProperty(ref _selectedGraph, value);
-		}
+    public Graph SelectedGraph
+    {
+      get => _selectedGraph;
+      set => SetProperty(ref _selectedGraph, value);
+    }
 
-		protected override void OnNavigatedTo(NavigationEventArgs e)
-		{
-			_navArgs = (BreadcrumbTrail)e.Parameter;
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      _navArgs = (BreadcrumbTrail)e.Parameter;
 
-			base.OnNavigatedTo(e);
+      base.OnNavigatedTo(e);
 
-			Initialise(_navArgs.SelectedRepository);
-		}
+      Initialise(_navArgs.SelectedRepository);
+    }
 
-		private async void Initialise(Repository repo)
-		{
-			var graphServer = new GraphServer(_config, _navArgs.Token, _innerHandler);
-			var graphs = await graphServer.ByOwners(new[] { repo.Id });
-			graphs.ToList()
-		  .ForEach(graph => Graphs.Add(graph));
-		}
+    private async void Initialise(Repository repo)
+    {
+      var graphServer = new GraphServer(_config, _navArgs.Token, _innerHandler);
+      var graphs = await graphServer.ByOwners(new[] { repo.Id });
+      graphs.ToList()
+      .ForEach(graph => Graphs.Add(graph));
+    }
 
-		private void Graph_Click(object sender, object args)
-		{
-			_navArgs.SelectedGraph = SelectedGraph;
-			// TODO	  Frame.Navigate(typeof(GraphItemPage), _navArgs); aka NodesEdgesPage
-		}
+    private void Graph_Click(object sender, object args)
+    {
+      _navArgs.SelectedGraph = SelectedGraph;
+      // TODO	  Frame.Navigate(typeof(GraphItemPage), _navArgs); aka NodesEdgesPage
+    }
 
-		private void Back_Click(object sender, object args)
-		{
-			Frame.Navigate(typeof(RepositoryPage), _navArgs);
-		}
-	}
+    private void Back_Click(object sender, object args)
+    {
+      Frame.Navigate(typeof(RepositoryPage), _navArgs);
+    }
+  }
 }
