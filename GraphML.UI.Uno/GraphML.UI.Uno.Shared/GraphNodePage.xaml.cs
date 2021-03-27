58ca878704b5e2b165f03c1d14d8303f949dc9ac
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-03-27 14:31:13 +0000

    - skeletons of graph-nodes + graph-edges pages

---
+++ GraphML.UI.Uno/GraphML.UI.Uno.Shared/GraphNodePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace GraphML.UI.Uno
+{
+	using Windows.UI.Xaml.Controls;
+	using Windows.UI.Xaml.Navigation;
+
+	public sealed partial class GraphNodePage : PageBase
+	{
+		public GraphNodePage()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			_navArgs = (BreadcrumbTrail)e.Parameter;
+
+			base.OnNavigatedTo(e);
+
+			Initialise(_navArgs.SelectedGraph);
+		}
+
+		private async void Initialise(Graph graph)
+		{
+			// TODO	Initialise
+		}
+
+		private void Back_Click(object sender, object args)
+		{
+			Frame.Navigate(typeof(GraphPage), _navArgs);
+		}
+	}
+}
