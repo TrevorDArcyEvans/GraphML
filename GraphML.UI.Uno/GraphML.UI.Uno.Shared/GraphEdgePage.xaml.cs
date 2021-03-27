2bcc83240317c7e7e3d435b781713bb7ffa0cd9b
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-03-27 17:05:35 +0000

    - refactored into GraphItemPageBase

--- GraphML.UI.Uno/GraphML.UI.Uno.Shared/GraphEdgePage.xaml.cs
+++ GraphML.UI.Uno/GraphML.UI.Uno.Shared/GraphEdgePage.xaml.cs
@@ -1,40 +1,15 @@
 ﻿namespace GraphML.UI.Uno
 {
 	using GraphML.UI.Uno.Server;
-	using System.Collections.ObjectModel;
 	using System.Linq;
-	using Windows.UI.Xaml.Controls;
-	using Windows.UI.Xaml.Navigation;
 
-	public sealed partial class GraphEdgePage : PageBase
+	public sealed partial class GraphEdgePage : GraphItemPageBase
 	{
-		public GraphEdgePage()
+		protected override async void InitialiseUI(Graph graph)
 		{
-			InitializeComponent();
-		}
-
-		public ObservableCollection<GraphItem> GraphItems { get; set; } = new ObservableCollection<GraphItem>();
-
-		protected override void OnNavigatedTo(NavigationEventArgs e)
-		{
-			_navArgs = (BreadcrumbTrail)e.Parameter;
-
-			base.OnNavigatedTo(e);
-
-			Initialise(_navArgs.SelectedGraph);
-		}
-
-		private async void Initialise(Graph graph)
-		{
-			var graphItemServer = new GraphEdgeServer(_config, _navArgs.Token, _innerHandler);
-			var graphItems = await graphItemServer.ByOwners(new[] { graph.Id });
-			graphItems.ToList()
-				.ForEach(graphItem => GraphItems.Add(graphItem));
-		}
-
-		private void Back_Click(object sender, object args)
-		{
-			Frame.Navigate(typeof(GraphPage), _navArgs);
+			var repoItems = await GraphEdgeServer.ByOwners(new[] { graph.Id });
+			repoItems.ToList()
+		  .ForEach(repoItem => MarshallToUI(() => GraphItems.Add(repoItem)));
 		}
 	}
 }
---
+++ GraphML.UI.Uno/GraphML.UI.Uno.Shared/GraphItemPageBase.xaml.cs
@@ -0,0 +1,44 @@
+﻿using GraphML.UI.Uno.Server;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace GraphML.UI.Uno
+{
+    public abstract partial class GraphItemPageBase : PageBase
+	{
+		protected GraphNodeServer GraphNodeServer;
+		protected GraphEdgeServer GraphEdgeServer;
+
+		public GraphItemPageBase()
+		{
+			InitializeComponent();
+		}
+
+		public ObservableCollection<GraphItem> GraphItems { get; set; } = new ObservableCollection<GraphItem>();
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			_navArgs = (BreadcrumbTrail)e.Parameter;
+
+			base.OnNavigatedTo(e);
+
+			Initialise(_navArgs.SelectedGraph);
+		}
+
+		protected abstract void InitialiseUI(Graph graph);
+
+		private async void Initialise(Graph graph)
+		{
+			GraphNodeServer = new GraphNodeServer(_config, _navArgs.Token, _innerHandler);
+			GraphEdgeServer = new GraphEdgeServer(_config, _navArgs.Token, _innerHandler);
+
+			InitialiseUI(graph);
+		}
+
+		private void Back_Click(object sender, object args)
+		{
+			Frame.Navigate(typeof(GraphPage), _navArgs);
+		}
+	}
+}
