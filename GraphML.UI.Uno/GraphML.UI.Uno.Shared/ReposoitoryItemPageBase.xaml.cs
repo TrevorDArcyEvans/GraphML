28d00ecfa50df2177f17914f0527a63ff1504eb5
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-03-28 21:49:44 +0100

    - added previous+next buttons

--- GraphML.UI.Uno/GraphML.UI.Uno.Shared/ReposoitoryItemPageBase.xaml.cs
+++ GraphML.UI.Uno/GraphML.UI.Uno.Shared/ReposoitoryItemPageBase.xaml.cs
@@ -1,49 +1,73 @@
 ﻿namespace GraphML.UI.Uno
 {
-	using GraphML.UI.Uno.Server;
-	using System.Collections.Generic;
-	using System.Collections.ObjectModel;
-	using System.Linq;
-	using System.Threading.Tasks;
-	using Windows.UI.Xaml.Controls;
-	using Windows.UI.Xaml.Navigation;
+  using GraphML.UI.Uno.Server;
+  using System.Collections.Generic;
+  using System.Collections.ObjectModel;
+  using System.Linq;
+  using System.Threading.Tasks;
+  using Windows.UI.Xaml.Controls;
+  using Windows.UI.Xaml.Navigation;
 
-	public abstract partial class ReposoitoryItemPageBase : PageBase
-	{
-		protected NodeServer NodeServer;
-		protected EdgeServer EdgeServer;
+  public abstract partial class ReposoitoryItemPageBase : PageBase
+  {
+    protected NodeServer NodeServer;
+    protected EdgeServer EdgeServer;
 
-		public ReposoitoryItemPageBase()
-		{
-			InitializeComponent();
-		}
+    public ReposoitoryItemPageBase()
+    {
+      InitializeComponent();
+    }
 
-		public ObservableCollection<RepositoryItem> RepositoryItems { get; set; } = new ObservableCollection<RepositoryItem>();
+    public ObservableCollection<RepositoryItem> RepositoryItems { get; set; } = new ObservableCollection<RepositoryItem>();
 
-		protected override void OnNavigatedTo(NavigationEventArgs e)
-		{
-			_navArgs = (BreadcrumbTrail)e.Parameter;
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      _navArgs = (BreadcrumbTrail)e.Parameter;
 
-			base.OnNavigatedTo(e);
+      base.OnNavigatedTo(e);
 
-			Initialise(_navArgs.SelectedRepository);
-		}
+      Initialise(_navArgs.SelectedRepository);
+    }
 
-		protected abstract Task<IEnumerable<RepositoryItem>> GetRepositoryItems(Repository repo, int pageIndex, int pageSize);
+    protected abstract Task<IEnumerable<RepositoryItem>> GetRepositoryItems(Repository repo, int pageIndex, int pageSize);
 
-		private async void Initialise(Repository repo)
-		{
-			EdgeServer = new EdgeServer(_config, _navArgs.Token, _innerHandler);
-			NodeServer = new NodeServer(_config, _navArgs.Token, _innerHandler);
+    private async Task Initialise(Repository repo)
+    {
+      EdgeServer = new EdgeServer(_config, _navArgs.Token, _innerHandler);
+      NodeServer = new NodeServer(_config, _navArgs.Token, _innerHandler);
 
-			var repoItems = await GetRepositoryItems(repo, _pageIndex, PageSize);
-			repoItems.ToList()
-		  .ForEach(repoItem => MarshallToUI(() => RepositoryItems.Add(repoItem)));
-		}
+      await LoadItems(repo);
+    }
 
-		protected void Back_Click(object sender, object args)
-		{
-			Frame.Navigate(typeof(RepositoryPage), _navArgs);
-		}
-	}
+    private async Task LoadItems(Repository repo)
+    {
+      RepositoryItems.Clear();
+
+      var repoItems = await GetRepositoryItems(repo, _pageIndex, PageSize);
+      repoItems.ToList()
+        .ForEach(repoItem => MarshallToUI(() => RepositoryItems.Add(repoItem)));
+    }
+
+    protected async Task Previous_Click(object sender, object args)
+    {
+      if (_pageIndex > 1)
+      {
+        _pageIndex--;
+      }
+
+      await LoadItems(_navArgs.SelectedRepository);
+    }
+
+    protected async Task Next_Click(object sender, object args)
+    {
+      _pageIndex++;
+
+      await LoadItems(_navArgs.SelectedRepository);
+    }
+
+    protected void Back_Click(object sender, object args)
+    {
+      Frame.Navigate(typeof(RepositoryPage), _navArgs);
+    }
+  }
 }
