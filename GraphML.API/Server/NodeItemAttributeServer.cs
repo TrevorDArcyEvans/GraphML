59cfed1c1d3f100783b874121dd25c43f9ff2701
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-04-06 08:52:24 +0100

    - formatting

--- GraphML.API/Server/NodeItemAttributeServer.cs
+++ GraphML.API/Server/NodeItemAttributeServer.cs
@@ -7,14 +7,14 @@ namespace GraphML.API.Server
   public sealed class NodeItemAttributeServer : OwnedItemServerBase<NodeItemAttribute>, INodeItemAttributeServer
   {
     public NodeItemAttributeServer(
-        IHttpContextAccessor httpContextAccessor,
-        IRestClientFactory clientFactory,
-        ILogger<NodeItemAttributeServer> logger,
-        ISyncPolicyFactory policy) :
-        base(httpContextAccessor, clientFactory, logger, policy)
+      IHttpContextAccessor httpContextAccessor,
+      IRestClientFactory clientFactory,
+      ILogger<NodeItemAttributeServer> logger,
+      ISyncPolicyFactory policy) :
+      base(httpContextAccessor, clientFactory, logger, policy)
     {
     }
 
-        protected override string ResourceBase { get; } = $"/api/{nameof(NodeItemAttribute)}";
+    protected override string ResourceBase { get; } = $"/api/{nameof(NodeItemAttribute)}";
   }
 }
