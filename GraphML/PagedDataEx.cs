025a99020cffb269405a12e83b4d7f9398a72f52
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-04-21 12:52:00 +0100

    - data structure support for MatTable

--- GraphML/PagedDataEx.cs
+++ GraphML/PagedDataEx.cs
@@ -6,14 +6,14 @@ namespace GraphML
   /// Data structure specifically intended for use with MatBlazor MatTable.
   /// MatTable will ask for a subset (aka page) of data from a potentially
   /// large set of data.  In order to implement paging (in the UI), MatTable
-  /// needs to know the <see cref="TotalCount"/> in all the data.
+  /// needs to know the <see cref="TotalCount"/> number of items
+  /// in all complete data.
   /// </summary>
   /// <typeparam name="T"></typeparam>
   public sealed class PagedDataEx<T>
   {
     /// <summary>
-    /// Total number of items in data query,
-    /// to assist in paging through all data
+    /// Total number of items in data query, to assist in paging through all data
     /// <remarks>
     /// BEWARE:  this is greater than or equal to <see cref="Items"/>
     /// </remarks>
