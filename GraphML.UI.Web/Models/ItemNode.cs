97540ad51b5c8e54378f9c46f2da51643b89e121
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-04-30 22:27:25 +0100

    - docs

--- GraphML.UI.Web/Models/ItemNode.cs
+++ GraphML.UI.Web/Models/ItemNode.cs
@@ -5,6 +5,12 @@ namespace GraphML.UI.Web.Models
 {
   public class ItemNode : NodeModel
   {
+    /// <summary>
+    /// A <see cref="Node"/> displayed in a <see cref="Chart"/>
+    /// </summary>
+    /// <param name="id">Unique identifier of underlying <see cref="Node"/></param>
+    /// <param name="name">Name of <see cref="Node"/></param>
+    /// <param name="pos">Location of <see cref="ItemNode"/> on <see cref="Chart"/></param>
     public ItemNode(string id, string name, Point pos) :
       base(id, pos)
     {
