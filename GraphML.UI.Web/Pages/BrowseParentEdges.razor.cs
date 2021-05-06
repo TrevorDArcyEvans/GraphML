4cca414a5d44950478bad84c41ff433a215f01f0
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-05-06 17:20:17 +0100

    - refactored to code-behind file

---
+++ GraphML.UI.Web/Pages/BrowseParentEdges.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GraphML.UI.Web.Pages
+{
+  public partial class BrowseParentEdges
+  {
+    #region Parameters
+
+    [Parameter]
+    public string OrganisationName { get; set; }
+
+    [Parameter]
+    public string OrganisationId { get; set; }
+
+    [Parameter]
+    public string RepositoryManagerName { get; set; }
+
+    [Parameter]
+    public string RepositoryManagerId { get; set; }
+
+    [Parameter]
+    public string RepositoryName { get; set; }
+
+    [Parameter]
+    public string RepositoryId { get; set; }
+
+    [Parameter]
+    public string ItemName { get; set; }
+
+    [Parameter]
+    public string ItemId { get; set; }
+
+    #endregion
+
+    private RepositoryItem[] _items;
+
+    private void GotoBrowseItems()
+    {
+      _navMgr.NavigateTo($"/BrowseItems/{OrganisationId}/{OrganisationName}/{RepositoryManagerId}/{RepositoryManagerName}/{RepositoryId}/{RepositoryName}");
+    }
+  }
+}
