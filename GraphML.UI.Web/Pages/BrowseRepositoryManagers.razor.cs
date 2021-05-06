15b062182086232d98d25f5e78270ba2ea13fb39
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-05-06 17:24:19 +0100

    - refactored to code-behind file

---
+++ GraphML.UI.Web/Pages/BrowseRepositoryManagers.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GraphML.UI.Web.Pages
+{
+  public partial class BrowseRepositoryManagers
+  {
+    [Parameter]
+    public string OrganisationName { get; set; }
+
+    [Parameter]
+    public string OrganisationId { get; set; }
+
+    private RepositoryManager[] _repoMgrs;
+
+    private void GotoBrowseRepositories(RepositoryManager repoMgr)
+    {
+      _navMgr.NavigateTo($"/BrowseRepositories/{OrganisationId}/{OrganisationName}/{repoMgr.Id}/{repoMgr.Name}");
+    }
+
+    private void GotoImporter()
+    {
+      _navMgr.NavigateTo($"/Importer/{OrganisationId}/{OrganisationName}");
+    }
+
+    private void GotoBrowseOrganisations()
+    {
+      _navMgr.NavigateTo($"/BrowseOrganisations");
+    }
+  }
+}
