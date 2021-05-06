a51e8588cdbdb93476349316b3461fd9a2a51d17
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-05-06 17:23:19 +0100

    - refactored to code-behind file

---
+++ GraphML.UI.Web/Pages/BrowseWhereUsedNodes.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GraphML.UI.Web.Pages
+{
+  public partial class BrowseWhereUsedNodes
+  {
+    #region Parameters
+
+    [Parameter]
+    public string OrganisationName { get; set; }
+
+    [Parameter]
+    public string OrganisationId { get; set; }
+
+    [Parameter]
+    public string RepositoryManagerName { get; set; }
+
+    [Parameter]
+    public string RepositoryManagerId { get; set; }
+
+    [Parameter]
+    public string RepositoryName { get; set; }
+
+    [Parameter]
+    public string RepositoryId { get; set; }
+
+    [Parameter]
+    public string ItemId { get; set; }
+
+    [Parameter]
+    public string ItemName { get; set; }
+
+    #endregion
+
+    private Graph[] _graphs;
+
+    private void GotoBrowseItems()
+    {
+      _navMgr.NavigateTo($"/BrowseItems/{OrganisationId}/{OrganisationName}/{RepositoryManagerId}/{RepositoryManagerName}/{RepositoryId}/{RepositoryName}");
+    }
+  }
+}
