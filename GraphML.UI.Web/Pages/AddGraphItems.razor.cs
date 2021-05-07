8d99b59bf95fc4d500533372ee3a8eeb2fd595d8
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-05-07 14:31:48 +0100

    - added AddGraphItems page

---
+++ GraphML.UI.Web/Pages/AddGraphItems.razor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlazorTable;
+using Microsoft.AspNetCore.Components;
+
+namespace GraphML.UI.Web.Pages
+{
+  public partial class AddGraphItems
+  {
+    #region Parameters
+
+    [Parameter]
+    public string OrganisationName { get; set; }
+
+    [Parameter]
+    public string OrganisationId { get; set; }
+
+    [Parameter]
+    public string RepositoryManagerName { get; set; }
+
+    [Parameter]
+    public string RepositoryManagerId { get; set; }
+
+    [Parameter]
+    public string RepositoryName { get; set; }
+
+    [Parameter]
+    public string RepositoryId { get; set; }
+
+    [Parameter]
+    public string GraphName { get; set; }
+
+    [Parameter]
+    public string GraphId { get; set; }
+
+    #endregion
+
+    private Node[] _data;
+    private Table<Node> _table;
+
+    protected override async Task OnInitializedAsync()
+    {
+      var dataPage = await _nodeServer.ByOwner(Guid.Parse(RepositoryId), 0, int.MaxValue, null);
+      _data = dataPage.Items.ToArray();
+    }
+
+    private void AddSelectedGraphItems()
+    {
+      var selItems = _table.SelectedItems;
+    }
+
+    private void GotoBrowseGraphItems()
+    {
+      _navMgr.NavigateTo($"/BrowseGraphItems/{OrganisationId}/{OrganisationName}/{RepositoryManagerId}/{RepositoryManagerName}/{RepositoryId}/{RepositoryName}/{GraphId}/{GraphName}");
+    }
+  }
+}
--- GraphML.UI.Web/Pages/BrowseGraphItems.razor.cs
+++ GraphML.UI.Web/Pages/BrowseGraphItems.razor.cs
@@ -35,6 +35,11 @@ namespace GraphML.UI.Web.Pages
     private Node[] _nodes;
     private Edge[] _edges;
 
+    private void GotoAddGraphItems()
+    {
+      _navMgr.NavigateTo($"/AddGraphItems/{OrganisationId}/{OrganisationName}/{RepositoryManagerId}/{RepositoryManagerName}/{RepositoryId}/{RepositoryName}/{GraphId}/{GraphName}");
+    }
+
     private void GotoBrowseGraphs()
     {
       _navMgr.NavigateTo($"/BrowseGraphs/{OrganisationId}/{OrganisationName}/{RepositoryManagerId}/{RepositoryManagerName}/{RepositoryId}/{RepositoryName}");
