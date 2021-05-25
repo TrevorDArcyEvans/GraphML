365408e4476264cd7bc92e813e5ec0adbf2103ed
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-05-25 21:58:34 +0100

    - added missing parameters

---
+++ GraphML.UI.Web/Pages/Visualisations/FindShortestPaths.razor.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Components;
+
+namespace GraphML.UI.Web.Pages.Visualisations
+{
+  public partial class FindShortestPaths
+  {
+    #region Parameters
+
+    [Parameter]
+    public string OrganisationName { get; set; }
+
+    [Parameter]
+    public string OrganisationId { get; set; }
+
+    [Parameter]
+    public string RepositoryManagerName { get; set; }
+
+    [Parameter]
+    public string RepositoryManagerId { get; set; }
+
+    [Parameter]
+    public string RepositoryName { get; set; }
+
+    [Parameter]
+    public string RepositoryId { get; set; }
+
+    [Parameter]
+    public string GraphName { get; set; }
+
+    [Parameter]
+    public string GraphId { get; set; }
+
+    [Parameter]
+    public string CorrelationId { get; set; }
+
+    #endregion
+    
+  }
+}
