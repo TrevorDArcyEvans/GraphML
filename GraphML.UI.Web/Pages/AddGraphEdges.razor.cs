edce96ca533ee54972cb7b0ae4c763427adfda9c
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-06-03 17:09:48 +0100

    - code cleanup

--- GraphML.UI.Web/Pages/AddGraphEdges.razor.cs
+++ GraphML.UI.Web/Pages/AddGraphEdges.razor.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BlazorTable;
 using GraphML.Interfaces.Server;
 using GraphML.UI.Web.Widgets;
 using GraphML.Utils;
